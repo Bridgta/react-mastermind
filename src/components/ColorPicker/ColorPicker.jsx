13f8126e206cda49dc025d877d3600eca290cc93
Bridget Asser <bridgetasser@gmail.com>
2020-02-04 23:37:47 -0500

    components added for game play and setting state

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import GuessRow from '../GuessRow/GuessRow';
+import ScoreButton from '../ScoreButton/ScoreButton';
 
-const ColorPicker = (props) => (
-    <div className='component'>
-        ColorPicker
+const GameBoard = (props) => (
+    <div>
+        {props.guesses.map((guess, idx) =>
+            <GuessRow
+                guess={guess}
+                colors={props.colors}
+                currentGuess={idx === (props.guesses.length - 1)}
+                rowIdx={idx}
+                key={idx}
+            />
+        )}
     </div>
 );
 
-export default ColorPicker;
+export default GameBoard;
